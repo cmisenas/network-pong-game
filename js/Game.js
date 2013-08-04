8e02f755629a314c14913cf9da0d7ac8fc391111
cmisenas <acolyte.camille@gmail.com>
2013-08-04 13:24:18 -0400

    Moved Game object to Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,10 @@
 function Game() {
+  this.players = [];
+}
 
+Game.prototype.setPlayAreaDimensions = function(width, height) {
+  this.width = width;
+  this.height = height;
 }
 
 module.exports = Game;
--- js/server.js
+++ js/server.js
@@ -10,123 +10,113 @@ var http = require('http'),
 
 var Player = require('./Players'),
     Ball = require('./Ball');
+    Game = require('./Game');
 
-var Game = function() {
-  this.players = [];
-};
+Game.prototype.addPlayer = function(client, data) {
+  if(this.players.length <= 2) {
+    //only set the canvasWidth and canvasHeight with the first player
+    if(this.players.length === 0){
+      this.setPlayAreaDimensions(data.canvasWidth, data.canvasHeight);
+      this.ball = new Ball(this.width, this.height);
+    }
 
-Game.prototype = {
-  setPlayAreaDimensions: function(width, height) {
-    this.width = width;
-    this.height = height;
-  },
+    // this could be cleaned up
+    // just make a player obj, assign to player, push onto this.players,
+    // then setting x, y, width etc can be done on player
 
-  addPlayer: function(client, data) {
-	  if(this.players.length <= 2) {
-		  //only set the canvasWidth and canvasHeight with the first player
-		  if(this.players.length === 0){
-        this.setPlayAreaDimensions(data.canvasWidth, data.canvasHeight);
-        this.ball = new Ball(this.width, this.height);
-		  }
+    // id2 seems redundant.  I think it can be deleted.
 
-      // this could be cleaned up
-      // just make a player obj, assign to player, push onto this.players,
-      // then setting x, y, width etc can be done on player
+    // why do you broadcast/emit player ids that are their array indices,
+    // rather than just using their client.id?
 
-      // id2 seems redundant.  I think it can be deleted.
+    var playerId = client.id;
+    this.players.push({id: playerId});
 
-      // why do you broadcast/emit player ids that are their array indices,
-      // rather than just using their client.id?
+    var playerIndex = findIndexById(playerId);
+    this.players[playerIndex].x = data.x;
+    this.players[playerIndex].y = (this.players.length === 1? data.canvasHeight - data.height - 5: 5);
+    this.players[playerIndex].width = data.width;
+    this.players[playerIndex].height = data.height;
+    this.players[playerIndex].score = 0;
 
-		  var playerId = client.id;
-		  this.players.push({id: playerId});
+    var nth = (this.players.length > 1? 2: 1);
+    this.players[playerIndex].nth = nth;
 
-		  var playerIndex = findIndexById(playerId);
-		  this.players[playerIndex].x = data.x;
-		  this.players[playerIndex].y = (this.players.length === 1? data.canvasHeight - data.height - 5: 5);
-		  this.players[playerIndex].width = data.width;
-		  this.players[playerIndex].height = data.height;
-		  this.players[playerIndex].score = 0;
+    //broadcast to other players about new player
+    if(this.players.length > 1){
+      client.broadcast.emit('new player', {id: playerIndex, id2: playerId, x: data.x});
+    }
 
-		  var nth = (this.players.length > 1? 2: 1);
-		  this.players[playerIndex].nth = nth;
+    //send new player data about existing players
+    for(var i = 0; i < this.players.length; i++){
+      if(this.players[i].id !== playerId)
+        client.emit('new player', {id: i, id2: this.players[i].id, x: this.players[i].x, nth: 1});
+    }
 
-		  //broadcast to other players about new player
-		  if(this.players.length > 1){
-			  client.broadcast.emit('new player', {id: playerIndex, id2: playerId, x: data.x});
-		  }
+    client.emit('assign player', {nth: nth});
+    client.emit('create ball', {x: this.ball.x, y: this.ball.y});
 
-		  //send new player data about existing players
-		  for(var i = 0; i < this.players.length; i++){
-			  if(this.players[i].id !== playerId)
-				  client.emit('new player', {id: i, id2: this.players[i].id, x: this.players[i].x, nth: 1});
-		  }
-
-		  client.emit('assign player', {nth: nth});
-		  client.emit('create ball', {x: this.ball.x, y: this.ball.y});
-
-		  if(this.players.length > 1){
-        this.start();
-		  }
-	  }
-  },
-
-  start: function() {
-    var time = Date.now();
-    var self = this;
-	  var loop = setInterval(function() {
-	    self.ball.update((Date.now() - time)/1000);
-
-	    for(var i = 0, maxPlayers = self.players.length; i < maxPlayers; i++){
-		    var nth = parseInt(self.players[i].nth);
-		    var playerYToCompare = nth === 1 ?
-          self.players[i].y :
-          self.players[i].y + self.players[i].height;
-
-		    if(nth === 1){
-			    var yCompared = self.ball.y + self.ball.r >= playerYToCompare;
-		    }else{
-			    var yCompared = self.ball.y - self.ball.r <= playerYToCompare;
-		    }
-
-		    if ((self.ball.x + self.ball.r >= self.players[i].x &&
-			       self.ball.x - self.ball.r <= self.players[i].x + self.players[i].width) &&
-				    yCompared) {
-			    self.players[i].score++;
-			    self.ball.directionY *= -1;
-			    socket.sockets.emit('update score', {nth: nth, score: self.players[i].score});
-		    }
-	    }
-
-	    socket.sockets.emit('move ball', {x: self.ball.x, y: self.ball.y});
-
-	    if(!self.ball.isInPlayArea()){
-		    console.log('Game Over');
-		    clearInterval(loop);
-		    socket.sockets.emit('game over', {msg: 'Game Over'});
-	    }
-
-	    time = Date.now();
-    }, 50);
-  },
-
-  movePlayer: function(client, data) {
-	  var playerId = client.id;
-	  var playerIndex = findIndexById(playerId);
-	  this.players[playerIndex].x = data.x;
-	  client.broadcast.emit('player moved', {x: data.x});
-  },
-
-  removePlayer: function(client) {
-	  //remove from players array
-	  var playerId = client.id;
-	  this.players.splice(findIndexById(playerId), 1);
-	  //broadcast to other players that client disconnected
-	  if(this.players.length > 1) {
-		  client.broadcast.emit('player disconnected', {id: playerId});
+    if(this.players.length > 1){
+      this.start();
     }
   }
-};
+}
+
+Game.prototype.start = function() {
+  var time = Date.now();
+  var self = this;
+  var loop = setInterval(function() {
+    self.ball.update((Date.now() - time)/1000);
+
+    for(var i = 0, maxPlayers = self.players.length; i < maxPlayers; i++){
+      var nth = parseInt(self.players[i].nth);
+      var playerYToCompare = nth === 1 ?
+        self.players[i].y :
+        self.players[i].y + self.players[i].height;
+
+      if(nth === 1){
+        var yCompared = self.ball.y + self.ball.r >= playerYToCompare;
+      }else{
+        var yCompared = self.ball.y - self.ball.r <= playerYToCompare;
+      }
+
+      if ((self.ball.x + self.ball.r >= self.players[i].x &&
+           self.ball.x - self.ball.r <= self.players[i].x + self.players[i].width) &&
+          yCompared) {
+        self.players[i].score++;
+        self.ball.directionY *= -1;
+        socket.sockets.emit('update score', {nth: nth, score: self.players[i].score});
+      }
+    }
+
+    socket.sockets.emit('move ball', {x: self.ball.x, y: self.ball.y});
+
+    if(!self.ball.isInPlayArea()){
+      console.log('Game Over');
+      clearInterval(loop);
+      socket.sockets.emit('game over', {msg: 'Game Over'});
+    }
+
+    time = Date.now();
+  }, 50);
+}
+
+Game.prototype.movePlayer = function(client, data) {
+  var playerId = client.id;
+  var playerIndex = findIndexById(playerId);
+  this.players[playerIndex].x = data.x;
+  client.broadcast.emit('player moved', {x: data.x});
+}
+
+Game.prototype.removePlayer = function(client) {
+  //remove from players array
+  var playerId = client.id;
+  this.players.splice(findIndexById(playerId), 1);
+  //broadcast to other players that client disconnected
+  if(this.players.length > 1) {
+    client.broadcast.emit('player disconnected', {id: playerId});
+  }
+}
 
 var serveStaticFile = function(filename, type, res) {
   fs.readFile(filename, 'utf8', function (err, data) {
