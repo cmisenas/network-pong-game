723a96d319218b0733a1038100b7e2be48a722cc
cmisenas <acolyte.camille@gmail.com>
2013-08-04 14:41:39 -0400

    More properties added to Players object

--- js/Players.js
+++ js/Players.js
@@ -1,11 +1,12 @@
-function Player(x, n) {
-	this.x = x;
-	this.y = null;
+function Player(n, w, h) {
   this.num = n;
 	this.width = 50;
   this.height = 5;
+	this.x = w/2 - this.width/2;
+	this.y = this.num === 1 ? h - this.height - 5 : 5;
   this.score = 0;
   this.vx = 200;
 }
 
 module.exports = Player;
+this.Player = Player;
