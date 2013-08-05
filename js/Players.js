a49f53f6d7054dfec8b405f4b8e08b3126985f22
cmisenas <acolyte.camille@gmail.com>
2013-08-05 09:40:13 -0400

    Corrected the width and height of players in server same as in client

--- js/Players.js
+++ js/Players.js
@@ -1,7 +1,7 @@
 function Player(n, w, h) {
   this.num = n;
-	this.width = 50;
-  this.height = 5;
+	this.width = 70;
+  this.height = 10;
 	this.x = w/2 - this.width/2;
 	this.y = this.num === 1 ? h - this.height - 5 : 5;
   this.score = 0;
