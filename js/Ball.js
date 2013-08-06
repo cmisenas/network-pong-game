8188255c657a2f370d9e7f696ae9419059402fbc
cmisenas <acolyte.camille@gmail.com>
2013-08-06 10:18:00 -0400

    Modified Ball object, canvas width and height needs to be public so prototype can access them Cannot have an update method for object, must be implemented individually by client and server in prototype

--- js/Ball.js
+++ js/Ball.js
@@ -1,27 +1,20 @@
 ;(function(exports) {
   function Ball(w, h) {
-    var CANVAS_WIDTH = w;
-    var CANVAS_HEIGHT = h;
+    this.CANVAS_WIDTH = w;
+    this.CANVAS_HEIGHT = h;
 
     this.r = 7;
-    this.x = CANVAS_WIDTH/2;
-    this.y = CANVAS_HEIGHT/2;
+    this.x = this.CANVAS_WIDTH/2;
+    this.y = this.CANVAS_HEIGHT/2;
     this.vx = 200;
     this.vy = 200;
     this.directionX = (Math.random() > 0.5? 1: -1);
     this.directionY = (Math.random() > 0.5? 1: -1);
 
-    this.update = function(mod){
-      if(this.y - this.r <= 0 || this.y + this.r >= CANVAS_HEIGHT)
-        this.directionY *= -1;
-
-      this.x += (this.vx * this.directionX) * mod;
-      this.y += (this.vy * this.directionY) * mod;
-    }
 
     // return true when ball has betmeen floor and ceiling of game world
     this.isInPlayArea = function() {
-      return this.x - this.r > 0 && this.x + this.r < CANVAS_WIDTH;
+      return this.x - this.r > 0 && this.x + this.r < this.CANVAS_WIDTH;
     };
   }
 
